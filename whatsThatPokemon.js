b8460fc7414a6e517a442d7b0e026d30027363ff
Chia T <tham_oh@hotmail.com>
2017-12-20 01:04:34 -0600

    first commit

---
+++ whatsThatPokemon.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+
+    var url_img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/";
+    for(var x = 1; x <= 50; x++){
+        $('#containerLeft').append('<img id="'+x+'" src="'+url_img+''+x+'.png">');
+    };
+
+    var url_info = "https://pokeapi.co/api/v2/pokemon/";
+    // var url_info = "http://pokeapi.salestock.net/api/v2/pokemon/";
+
+    $('#containerLeft').on('click', 'img', function() {
+        var id = $(this).attr('id');
+        console.log(url_info+id);
+        $.get(url_info+id, function(pokemon) {
+            console.log(pokemon);
+            var HTMLstring = `
+                <div id='groupPhoto'>
+                    <img src='${pokemon.sprites.front_default}'>
+                    <img src='${pokemon.sprites.front_shiny}'>
+                </div>
+                <h1>Name: ${pokemon.name}</h1>
+                <p>Height: ${pokemon.height}</p>
+                <p>Weight: ${pokemon.weight}</p>
+                <p>Species: ${pokemon.species.name}</p>
+            `
+            $('#containerRight').html(HTMLstring);
+        },'json');
+    } )
+
+})
